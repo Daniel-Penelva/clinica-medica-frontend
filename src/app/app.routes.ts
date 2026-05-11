f0a54138d445efd562379655b54045c361b49807
Daniel Penelva <d4n.andrade@gmail.com>
2026-05-11 19:42:32 -0300

    feat: (Criando rotas para testar) Alterando add rotas no app.routes.ts + alterando app.component  .spec.ts, .ts e .html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    // Redireciona raiz para login por enquanto
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+    // Rota não encontrada
+    { path: '**', redirectTo: 'login'},
+
+];
